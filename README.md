38462c38a5414d81a0874dfc8447c3b6095825b6
CodeIgor31 <yagmurov.igor@mail.ru>
2024-04-04 00:31:27 +0300

    HW2: After review fixes

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # askme_yagmurov
-Для запуска проекта:<br>
+Для запуска проекта:
 
-python3 -m venv askme_venv - создайте виртуальное окружение с помощью встроенного модуля venv<br>
-source askme_venv/bin/activate - активируйте виртуальное окружение(Linux и macOS)<br>
-python3 manage.py runserver - запустите сервер<br>
-Откройте браузер и перейдите по адресу http://127.0.0.1:8000/<br>
+python3 -m venv askme_venv - создайте виртуальное окружение с помощью встроенного модуля venv
+source askme_venv/bin/activate - активируйте виртуальное окружение(Linux и macOS)
+pip install -r requirements.txt - установите зависимости
+python3 manage.py runserver - запустите сервер
+Откройте браузер и перейдите по адресу http://127.0.0.1:8000/
--- app/views.py
+++ app/views.py
@@ -29,6 +29,25 @@ ANSWERS = [
     } for i in range(20)
 ]
 
+def generate_page_urls(request, page_obj):
+    path = request.path
+    previous_page_url = ''
+    next_page_url = ''
+
+    query_params = request.GET.copy()
+    query_params.pop('page', None)
+
+    if page_obj.has_previous():
+      query_params['page'] = page_obj.previous_page_number()
+      previous_page_url = f"{path}?{query_params.urlencode()}"
+
+    if page_obj.has_next():
+      query_params.pop('page', None)
+      query_params['page'] = page_obj.next_page_number()
+      next_page_url = f"{path}?{query_params.urlencode()}"
+
+    return previous_page_url, next_page_url
+
 def paginate(objects, request, per_page=5):
     page_num = request.GET.get('page', 1)
     paginator = Paginator(objects, per_page)
@@ -42,18 +61,41 @@ def paginate(objects, request, per_page=5):
 
 def index(request):
   page_obj = paginate(QUESTIONS, request, 3)
+  previous_page_url, next_page_url = generate_page_urls(request, page_obj)
+  context = {
+    "questions": page_obj,
+    "previous_page_url": previous_page_url,
+    "next_page_url": next_page_url,
+    "is_auth": 1
+    }
 
-  return render(request, "index.html", { "questions": page_obj, "is_auth": 1 })
+  return render(request, "index.html", context)
 
 def hot(request):
   page_obj = paginate(BENDER_QUESTIONS, request, 3)
+  previous_page_url, next_page_url = generate_page_urls(request, page_obj)
+  context = {
+    "questions": page_obj,
+    "previous_page_url": previous_page_url,
+    "next_page_url": next_page_url,
+    "is_auth": 1
+    }
 
-  return render(request, "hot.html", { "questions": page_obj, "is_auth": 1 })
+  return render(request, "hot.html", context)
 
 def question(request, question_id):
   item = QUESTIONS[question_id]
   page_obj = paginate(ANSWERS, request, 3)
-  return render(request, "question.html", { "question": item, "answers": page_obj, "is_auth": 1 })
+  previous_page_url, next_page_url = generate_page_urls(request, page_obj)
+  context = {
+    "question": item,
+    "answers": page_obj,
+    "previous_page_url": previous_page_url,
+    "next_page_url": next_page_url,
+    "is_auth": 1
+    }
+
+  return render(request, "question.html", context)
 
 def ask(request):
   return render(request, "ask.html", { "is_auth": 1 })
@@ -77,7 +119,16 @@ def tag(request, tag_name):
     return HttpResponse("No questions with this tag")
 
   page_obj = paginate(questions, request, 3)
-  return render(request, "tag.html", { "questions": page_obj, 'tag_name': tag_name, "is_auth": 1 })
+  previous_page_url, next_page_url = generate_page_urls(request, page_obj)
+  context = {
+    "questions": page_obj,
+    "tag_name": tag_name,
+    "previous_page_url": previous_page_url,
+    "next_page_url": next_page_url,
+    "is_auth": 1
+    }
+
+  return render(request, "tag.html", context)
 
 def logout(request):
   # Тут должен будет быть пост запрос и потом редирект в любом случае
